f8deceb940f314270e7f59de5ba7c9d62dabfc33
KSSJW <ZYJ2352401938@163.com>
2025-11-22 00:14:40 +0800

    1.20-release.1 (Chore) Adjust Mixin injection signatures for version range 1.20 - 1.20.1.

--- src/client/java/com/kssjw/glowingminecart/client/mixin/MinecartPlaceMixin.java
+++ src/client/java/com/kssjw/glowingminecart/client/mixin/MinecartPlaceMixin.java
@@ -16,7 +16,7 @@ import net.minecraft.util.math.BlockPos;
 @Mixin(ClientWorld.class)
 public abstract class MinecartPlaceMixin {
     @Inject(method = "addEntity", at = @At("TAIL"))
-    private void gm$onEntityAdded(Entity entity, CallbackInfo ci) {
+    private void gm$onEntityAdded(int id, Entity entity, CallbackInfo ci) {
         if (entity instanceof AbstractMinecartEntity) {
             BlockPos pos = entity.getBlockPos();
             DelayUtil.schedule(5, () -> ForceLightUpdateUtil.update(pos.getX(), pos.getY(), pos.getZ(), pos.getX(), pos.getY(), pos.getZ()));   // 矿车放置后延时刷新渲染
